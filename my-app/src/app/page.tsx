8670ded2d71882d0ee5a605c1df0ddcf83b063a7
haizeagc <116098300+haizeagc@users.noreply.github.com>
2025-06-04 13:38:13 +0200

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center p-8">
-      <div className="bg-white text-center p-4 border rounded shadow-md">
+    <div className="flex items-center justify-center p-8 min-h-screen bg-white">
+      <div className="text-center p-4 border rounded shadow-md">
         <h1 className="text-black font-bold">Nuestra Empresa</h1>
         <p className="text-sm text-gray-600">
           Somos una empresa dedicada a ofrecer soluciones tecnológicas innovadoras. 
