0201aa83467bbe940bfdfaab2d4511523584d010
haizeagc <116098300+haizeagc@users.noreply.github.com>
2025-06-07 17:39:29 +0200

    Paginas tarifas y catalogo

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -38,11 +38,9 @@ export default function RootLayout({
         {/* Menú horizontal con 5 apartados */}
         <nav className="bg-[#5b60ff] p-4 shadow-md fixed top-[32px] left-0 w-full z-40">
           <ul className="flex justify-center gap-8">
-            <li className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">INICIO</li>
-            <li className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">TARIFAS</li>
-            <li className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">MÁS INFORMACIÓN</li>
-            <li className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">CONTACTO</li>
-            <li className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">SEGURIDAD¿?</li>
+            <li> <a href= "" className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">INICIO </a></li>
+            <li> <a href= "/tarifas" className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">TARIFAS</a> </li>
+            <li> <a href= "/catalogo" className="text-sm font-normal text-white hover:font-bold hover:text-black active:text-violet-300 cursor-pointer">CATALOGO</a> </li>
           </ul>
         </nav>
 
---
+++ my-app/src/app/tarifas/page.tsx
@@ -0,0 +1,12 @@
+export default function Tarifas() {
+    return (
+      <div className="flex items-center justify-center p-8 min-h-screen bg-white">
+        <div className="text-center p-4 border rounded shadow-md">
+          <h1 className="text-black font-bold">Nuestra Empresa</h1>
+          <p className="text-sm text-gray-600">
+            TARIFAS DE LA EMPRESA
+          </p>
+        </div>
+      </div>
+    );
+  }
