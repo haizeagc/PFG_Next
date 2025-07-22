90ee2ace8f0927bf43cdf840165805cf58ef29da
haizeagc <116098300+haizeagc@users.noreply.github.com>
2025-07-22 22:01:37 +0200

    Filtros

--- my-app/src/app/ui/catalogo/filtro.tsx
+++ my-app/src/app/ui/catalogo/filtro.tsx
@@ -1,17 +1,25 @@
 export default function Filtro() {
   return (
     <div className="w-64 h-auto p-4 border rounded shadow-md bg-gray-100">
-      <h2 className="text-lg font-bold">Filtrooooo</h2>
-      <label htmlFor="transport">¿Cómo va a llegar hasta aquí?:</label>
-      <select name="transport" id="transport" className="mt-2 p-2 border rounded w-full">
-        <option value="">--Por favor, elija una opción--</option>
-        <option value="plane">Avión</option>
-        <option value="bike">Bicicleta</option>
-        <option value="walk">Caminar</option>
-        <option value="bus">Autobús</option>
-        <option value="train">Tren</option>
-        <option value="jetPack">Jet pack</option>
+      <h2 className="text-lg font-bold">Filtros</h2>
+      <label htmlFor="transport">Marca:</label>
+      <select name="Marca" id="marca" className="mt-2 p-2 border rounded w-full">
+        <option value="">Marca...</option>
+        <option value="SAMSUNG">Samsung</option>
+        <option value="APPLE">Apple</option>
+        <option value="XIAOMI">Xiaomi</option>
+        <option value="HONOR">Honor</option>
+        <option value="GOOGLE">Google</option>
+        <option value="MOTOROLA">Motorola</option>
+        <option value="ZTE">ZTE</option>
+        <option value="TCL">TCL</option>
+        <option value="OPPO">OPPO</option>
+        <option value="HONOR">Honor</option>
+        <option value="VIVO">Vivo</option>
       </select>
+      <p>Precio:</p>
+
+      <p>Memoria:</p>
     </div>
   );
 }
