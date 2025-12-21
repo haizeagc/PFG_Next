066b8a46b2b075755bb7207cb312f5bda1a8a659
haizeagc <116098300+haizeagc@users.noreply.github.com>
2025-12-21 17:33:40 +0100

    Visual de los datos moviles

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import ImagenesCarrusel from "./ui/imagenesMov";
 export default function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-center text-2xl mb-4">PAGINA PRINCIPAL</h1>
+      <h1 className="text-center text-3xl font-bold mb-4">PAGINA PRINCIPAL</h1>
 
       <ImagenesCarrusel/>
 
